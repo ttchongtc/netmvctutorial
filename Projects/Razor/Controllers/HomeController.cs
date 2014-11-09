83b93425bd62648a4678e5cb3c9b420dc39a5272
To Chong <chongtto@gmail.com>
2014-11-09 15:53:23 -0800

    check in Razor project

---
+++ Projects/Razor/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Razor.Models;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+        
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array = 
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Life Jacket", Price = 48.95M},
+                new Product {Name = "Soccer Ball", Price = 19.50M},
+                new Product {Name = "Corner Flag", Price = 34.95M},
+            };
+
+            return View(array); 
+        }
+    }
+}
