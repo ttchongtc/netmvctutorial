6848b1c5f92c23db0cfba4d1b8b54c870c85bf4e
To Chong <chongtto@gmail.com>
2014-11-10 09:10:04 -0800

    check in essentialtools

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EssentialTools.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products = {
+                                         new Product{ Name = "Kayak", Category = "Watersports", Price = 275M},
+                                         new Product{ Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                                         new Product{ Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                                         new Product{ Name = "Corner flag", Category = "Soccer", Price = 34.95M},
+                                     };
+
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+	}
+}
