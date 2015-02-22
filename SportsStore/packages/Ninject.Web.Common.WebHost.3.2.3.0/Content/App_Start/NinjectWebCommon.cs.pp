7781b9bcbbccebbe4441efbd0d63c863f38e7a9d
To Chong <chongtto@gmail.com>
2015-02-22 13:47:45 -0800

    Updated MVC version

--- SportsStore/packages/Ninject.Web.Common.WebHost.3.2.3.0/Content/App_Start/NinjectWebCommon.cs.pp
+++ SportsStore/packages/Ninject.Web.Common.WebHost.3.2.3.0/Content/App_Start/NinjectWebCommon.cs.pp
@@ -1,7 +1,7 @@
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(SportsStore.WebUI.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(SportsStore.WebUI.App_Start.NinjectWebCommon), "Stop")]
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof($rootnamespace$.App_Start.NinjectWebCommon), "Stop")]
 
-namespace SportsStore.WebUI.App_Start
+namespace $rootnamespace$.App_Start
 {
     using System;
     using System.Web;
